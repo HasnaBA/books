26bcac94aebde8afef0a872487743517efb7e9a5
Hasna Ambarki <h.bourajoini@gmail.com>
2019-10-31 17:00:07 +0100

    navbar2

--- public/index.php
+++ public/index.php
@@ -9,12 +9,13 @@
 
     <body>
 
-        <nav class="navbar navbar-light bg-light">
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </nav>
+    <nav  class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand"><h1>MyBooks</h1></a>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </nav>
 
         <?php echo $content ?>
 
