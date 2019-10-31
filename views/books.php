bab5fa37f0315488036f0b4aed65740c5255838f
Hasna Ambarki <h.bourajoini@gmail.com>
2019-10-31 16:02:10 +0100

    part2

--- views/books.php
+++ views/books.php
@@ -6,6 +6,25 @@
 <h1> Liste des livres </h1>
 <p> Bienvenue sur la page des livres</p>
 
+<ul>
+    <?php
+    foreach ($books as $book) {
+       
+        echo'<li>' . $book['title'] . '</li>';
+        
+    }
+    ?>
+</u>
+
+
+
+
+
+
+
+<pre>
+    <?php var_dump($books); ?>
+</pre>
 <!-- ending each page -->
 <?php $content=ob_get_clean();?>
 <?php require ('public/index.php');?>
