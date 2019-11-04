927f6dac4cf5390c7629677995ad9d6fda822973
Hasna Ambarki <h.bourajoini@gmail.com>
2019-11-04 17:02:02 +0100

    image et books

--- views/books.php
+++ views/books.php
@@ -3,29 +3,61 @@
 <?php ob_start(); ?>
 <!-- end of starting -->
 
-<h1> Liste des livres </h1>
-<p> Bienvenue sur la page des livres</p>
+<style>
+.autheur{
+    font-family:'Jomolhari', serif!important;
+    font-size: 20px;
+    font-weight: bolder;
+    text-align: center;
+    
+}
+.book_title{
+    font-family:'Jomolhari', serif!important;
+    font-size: 30px;
+    text-align: center;
+    
+}
+.titre{
+    font-family:'Jomolhari', serif!important;
+    font-size: 20px;
+    font-weight: bolder;
+    text-align: center;
+}
 
-<div class="container">   
-    <div class='row'> 
-        <div class="col-md-6">
-            <ul>
-            
-                <?php foreach ($books as $book) { ?>
-                
-                    <div class="card" style="width: 18rem;">
-                        <img src="<?php echo $books['imageLink']?>" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="<?php echo $book['id']; ?>"></h5>
-                                <p class="card-text"><?php echo $book['title']; ?></p>
-                                <a href="#" class="btn btn-primary">Ajouter au panier</a>
-                            </div>
+#size{
+    display: flex;
+    height: 300px;
+}
+
+#size img {
+    height: 100%;
+    width: 100%;
+}
+</style>
+
+<h1> Liste des livres </h1>
+
+<div class="container">     
+    <div class="row">
+        <?php foreach ($books as $book) { ?>
+            <div class="col-md-3 mt-4">
+                <div class="card h-100">
+                    <div id="size">
+                        <img src="<?php echo $book['imageLink']?>" class="card-img-top" alt="...">
                     </div>
-                <?php } ?>
-            </ul>
-        </div>
+                    <div class="card-body">
+                        <h5><?php echo $book['title']; ?></h5>
+                        <p class="card-text"><span class= "autheur" >Autheur: </span> <?php echo $book['author']; ?> </p>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="#" class="btn btn-primary">Ajouter au panier</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+        
     </div>
-</div>
+        </div>
 
 
 
