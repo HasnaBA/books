f49cd368deedf67aad253924d12d67e544c9b640
Hasna Ambarki <h.bourajoini@gmail.com>
2019-11-05 17:06:43 +0100

    pagination2

--- models/books.php
+++ models/books.php
@@ -1,16 +1,61 @@
 <?php
+
+
+
+
 require_once('utiles/db.php');
+
+
+ // starting pagination
+
+
+function countBooks ()
+{
+    $db = dbConnect(); //connexion base de donn'ée
+
+    //preparation de la requete
+    $stmt = $db->prepare('SELECT 
+        count(*) 
+        FROM `books` 
+    ');
+
+    $stmt->execute(); //execute
+
+    return $stmt->fetchColumn();//return fetchALL(retourn tout),fetchColumn(retoutnre le nbr de colonne)
+
+}
 
 function getBooks()
 {
+    $limit = 20;
+
+    $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+
+    $count = countBooks();
+    $offset = ($page - 1)  * $limit; // Calculate the offset for the query
+
+  
+
+   
+
     $db = dbConnect();
+   
+   
+
+    //ending pagination
+
+
     $stmt = $db->prepare('SELECT /* slectionne b.tout*/
         books.*,
         authors.name AS author
         FROM books
         LEFT JOIN authors 
         ON books.author_id = authors.id /*indique ou faire la correspondance*/
-    ');
+        LIMIT '. $offset .', '. $limit .'
+        ');
+
+    $stmt->bindParam(':offset' , $offset);
+    $stmt->bindParam(':limit' , $limit);
 
     $stmt->execute();
 
@@ -35,3 +80,4 @@ function getBook($id)
     return $stmt->fetch();
 
 }
+?>
